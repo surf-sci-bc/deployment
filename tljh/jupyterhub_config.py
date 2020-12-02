9c222768dab76e40fffc8332a90cd8a847b4b0b4
Simon Fischer <github@simon-fischer.info>
2020-12-03 00:59:42 +0100

    added tljh configuration file

---
+++ tljh/jupyterhub_config.py
@@ -0,0 +1,30 @@
+"""
+This goes into
+/opt/tljh/config/jupyterhub_config.d/
+"""
+
+from jupyter_client.localinterfaces import public_ips
+
+
+def get_docker_tags(repo_name):
+    try:
+        import requests
+        import json
+    except ImportError:
+        return ["latest"]
+    req = requests.get(f"http://localhost:5000/v2/{repo_name}/tags/list")
+    contents = json.loads(req.content)
+    return contents["tags"]
+
+
+c.JupyterHub.hub_ip = public_ips()[0]
+c.JupyterHub.cleanup_servers = False
+c.JupyterHub.spawner_class = "dockerspawner.SystemUserSpawner"
+c.SystemUserSpawner.host_homedir_format_string = "/home/jupyter-{username}"
+
+c.DockerSpawner.allowed_images = dict(
+    (tag, f"localhost:5000/agfalta_tools:{tag}")
+    for tag in get_docker_tags("agfalta_tools")
+)
+c.DockerSpawner.pull_policy = "Never"
+c.DockerSpawner.remove = True

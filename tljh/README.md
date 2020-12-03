e420e44cea87603a4af45d9af69f29c9c701f9d4
Simon Fischer <github@simon-fischer.info>
2020-12-03 14:48:25 +0100

    jupyterhub_config now makes adding users possible

---
+++ tljh/README.md
@@ -0,0 +1,8 @@
+# Configuration
+
+Put the config file into the right directory (assumes you are in this repo's root):
+
+```sh
+$ sudo cp tljh/jupyterhub_config.py /opt/tljh/config/jupyterhub_config.d
+$ sudo tljh-config reload
+```
--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -2,12 +2,34 @@
 This goes into
 /opt/tljh/config/jupyterhub_config.d/
 """
-from jupyter_client.localinterfaces import public_ips
-from dockerspawner import SystemUserSpawner
-from tornado import gen, web
+# pylint: disable=missing-docstring
+# pylint: disable=import-error
 
+import pwd
+from dockerspawner import SystemUserSpawner
+from firstuseauthenticator import FirstUseAuthenticator
+from jupyterhub.auth import LocalAuthenticator
+
+from jupyter_client.localinterfaces import public_ips
+from tornado import gen
+
+
+def convert_username(user_name):
+    return f"jupyter-{user_name}"
 
 class MyDockerSpawner(SystemUserSpawner):
+    def get_env(self):
+        env = super().get_env()
+        user_name = f"jupyter-{self.user.name}"
+        env.update(dict(USER=user_name, NB_USER=user_name))
+        return env
+
+    def _user_id_default(self):
+        return pwd.getpwnam(convert_username(self.user.name)).pw_uid
+
+    def _group_id_default(self):
+        return pwd.getpwnam(convert_username(self.user.name)).pw_gid
+
     @gen.coroutine
     def pull_image(self, image):
         """
@@ -26,6 +48,21 @@ class MyDockerSpawner(SystemUserSpawner):
         yield self.docker('pull', repo, tag)
         return
 
+class DummyUser:
+    # pylint: disable=too-few-public-methods
+    def __init__(self, user):
+        self.name = convert_username(user.name)
+
+class LocalFirstUseAuthenticator(FirstUseAuthenticator, LocalAuthenticator):
+    def system_user_exists(self, user):
+        user = DummyUser(user)
+        return super().system_user_exists(user)
+
+    def add_system_user(self, user):
+        user = DummyUser(user)
+        self.log.info(user)
+        return super().add_system_user(user)
+
 
 def get_docker_tags(repo_name):
     try:
@@ -37,18 +74,20 @@ def get_docker_tags(repo_name):
     contents = json.loads(req.content)
     return contents["tags"]
 
-
+# pylint: disable=undefined-variable
 c.JupyterHub.hub_ip = public_ips()[0]
 c.JupyterHub.cleanup_servers = False
+
+c.JupyterHub.spawner_class = MyDockerSpawner
 c.Spawner.default_url = "/lab"
 c.SystemUserSpawner.host_homedir_format_string = "/home/jupyter-{username}"
-
-# c.JupyterHub.spawner_class = "dockerspawner.SystemUserSpawner"
-c.JupyterHub.spawner_class = MyDockerSpawner
-
 c.DockerSpawner.image_whitelist = dict(
     (tag, f"localhost:5000/agfalta_tools:{tag}")
     for tag in get_docker_tags("agfalta_tools")
 )
 c.DockerSpawner.pull_policy = "always"
 c.DockerSpawner.remove = True
+
+c.JupyterHub.authenticator_class = LocalFirstUseAuthenticator
+c.FirstUseAuthenticator.create_users = False
+c.LocalAuthenticator.create_system_users = True

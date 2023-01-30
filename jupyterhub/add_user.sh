c25b03dfae61169103ac796d6ccc1354a8396f21
Lars Buß <lars1@uni-bremen.de>
2023-01-30 14:03:08 +0100

    Updated convenience scripts

--- jupyterhub/add_user.sh
+++ jupyterhub/add_user.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "Creating User jupyter-$1"
+if [[ -z $* ]] ; then
+    echo 'No user name was given.'
+    echo 'Please provide user name:'
+    read NAME
+else
+    NAME=$1
+fi
+echo "Creating User jupyter-$NAME"
 adduser -q --gecos "" --disabled-password jupyter-$1
-
-echo "Remember to restart Jupyterhub Container by:"
-echo "docker-compose restart jupyterhub"
+echo "Restarting jupyterhub to load new user"
+docker-compose restart jupyterhub
---
+++ jupyterhub/deploy_uspy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+uspy_docker_path="../docker/"
+
+echo "Bring up Registry."
+docker-compose up registry -d
+echo "Pull latest uspy"
+git -C $uspy_docker_path/uspy pull || git clone https://github.com/surf-sci-bc/uspy.git $uspy_docker_path/uspy
+echo "Checkout Tag $1"
+git -C $uspy_docker_path/uspy checkout $1
+echo "Done."
+echo "Building Docker image:"
+docker build -t localhost:5000/uspy:latest -t localhost:5000/uspy:$1 $uspy_docker_path
+echo "Push Images:"
+docker push localhost:5000/uspy:latest
+docker push localhost:5000/uspy:$1
+echo "Restart Jupyterhub."
+docker-compose restart
+echo "Done."
